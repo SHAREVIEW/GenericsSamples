804d1d67ca722828e3e9af698247edb992163f56
david <2657070472@qq.com>
2019-06-17 13:42:09 +0800

    314425 ch05 code

--- LinkedListSample/Program.cs
+++ LinkedListSample/Program.cs
@@ -16,17 +16,20 @@ namespace Wrox.ProCSharp.Generics
         Console.WriteLine(i);
       }
 
-      var list3 = new LinkedList<string>();
+      var list3 = new LinkedList<string>();   // no boxing - valnue types are stored in the List<int>
       list3.AddLast("2");
       list3.AddLast("four");
       list3.AddLast("foo");
-
+                                                                    // no boxing, no cast needed
       foreach (string s in list3)
       {
-        Console.WriteLine(s);
+        Console.WriteLine(s);     
       }
-
-
     }
   }
 }
+
+
+///List<T>类范类型定义为int，所以int类型在JIT编译器动态生成的类中使用，不再进行装箱和拆箱
+///拆箱、装箱操作容易，但性能损失较大
+///system.collections.generic名称空间的List<T>类不使用对象，而是在使用时定义类型。
